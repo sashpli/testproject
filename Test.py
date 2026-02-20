4ba476788f784eef367132f367a3a7548f47cc00
Sasa <sasapliso@outlook.com>
2026-02-20 16:19:00 +0100

    Initial Commit

---
+++ Test.py
@@ -0,0 +1,37 @@
+first_name = "Sasa"
+last_name = "Pliso"
+full_name = first_name + " " + last_name
+print(full_name)
+
+print(f"Hello {first_name} Would you like to learn pyhon today?")
+
+print(first_name.title())
+print(first_name.upper())
+print(first_name.lower())
+
+print('Albert Einstein once Seid "A person who never made a \nmistake never tried anything new"')
+
+famous_person = "Albert Einstein"
+print(f'{famous_person} once said, "A person who never made a mistake never tried anything new"')
+message = ' once said, "TEST  person who never made a \nmistake never tried anything new"'
+print(famous_person + message)
+
+new_name = " Wilhelm "
+print(new_name)
+print(new_name.strip())
+print(new_name.lstrip())
+print(new_name.rstrip())
+print(len(new_name.strip()))
+
+print(0.2 + 0.1)
+
+age = 23
+message_2 = "Heppy " + str(age) + " Birthday!"
+print(message_2)
+
+print(4 + 4)
+print(4 * 2)
+print(16 //2)
+print(10 - 2)
+
+favorite = "name"
