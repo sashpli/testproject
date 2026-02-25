384264b0d476dc739eb64253832629fbed740ff1
Sasa Pliso <sasapliso@icloud.com>
2026-02-25 20:20:01 +0100

    learning lists and loops

---
+++ numbers.py
@@ -0,0 +1,15 @@
+for value in range(1,6):
+    print(value)
+
+numbers = list(range(1,6))
+print(numbers)
+
+evennumbers = list(range(2, 10, 2))
+print(evennumbers)
+
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+
+print(squares)
