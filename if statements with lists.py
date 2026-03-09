37a4172e0b6fb2a2dab887242a81b420b4af5612
Sasa Pliso <sasapliso@icloud.com>
2026-03-09 19:40:37 +0100

    learning if statements and boolean expressions

--- if statements with lists.py
+++ if statements with lists.py
@@ -12,3 +12,21 @@ for requested_topping in requested_toppings:
     else:
         print(f"{requested_topping.title()} added to your pizza")
 print("Finished making your pizza!")
+
+print("\n")
+
+
+requested_toppings = []
+#starting with if statement when name of list is used in Python, it returns TRUE
+#if the list contains one item. Empty list is False, if toppings passes test then run for loop
+#if test FALSE then print ELSE statement
+if requested_toppings:
+    for requested_topping in requested_toppings:
+        print(f"Adding {requested_topping} to your pizza")
+    print("Finished making your pizza!")
+else:
+        print("Are you sure you want a plain pizza?")
+
+print("\n")
+#Using multiple Lists
+
