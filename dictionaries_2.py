161cacb42d2525feda30f45a7d6f178dd3842437
Sasa Pliso <sasapliso@icloud.com>
2026-03-19 19:27:46 +0100

    dictionaries and nesting

--- dictionaries_2.py
+++ dictionaries_2.py
@@ -87,3 +87,11 @@ print(f"\t{glossary['lists']}")
 print("\nPython:")
 print(f"\t{glossary['python']}")
 
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+for k, v in user_0.items():
+    print(f"Key: {k}")
+    print(f"Value: {v}")
