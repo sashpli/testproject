8f865017662c231ebff8632810a28cb369c66a6b
Sasa Pliso <sasapliso@icloud.com>
2026-03-19 18:06:03 +0100

    dictionaries and nesting

---
+++ dictionaries_2.py
@@ -0,0 +1,10 @@
+# alien.py
+
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+print(alien_0['points'])
+
+new_points = alien_0['points']
+print(f"You now have {new_points} points.")
+
+alien_0['x_position'] = 0
