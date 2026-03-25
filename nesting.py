dae8aefa5e73bed7b0a46538ff229772c6d2fa74
Sasa Pliso <sasapliso@icloud.com>
2026-03-25 18:45:07 +0100

    dictionaries and nesting

--- nesting.py
+++ nesting.py
@@ -26,3 +26,42 @@ for alien in aliens[:5]:
 print('...')
 #show how many aliens habe been created
 print(f"Total number of aliens {len(aliens)}")
+
+cars = []
+for car_number in range(20):
+    new_car = {'make': 'bmw', 'HP': 300, 'color': 'black'}
+    cars.append(new_car)
+
+for car in cars[:5]:
+    if car['make'] == 'bmw':
+        car['make'] = 'audi'
+        car['HP'] = 150
+        car['color'] = 'green'
+for car in cars[:10]:
+    print(car)
+
+# nesting foods
+foods = []
+
+for new_foods in range(15):
+    food = {'tye': 'banana', 'taste': 'sweet', 'color': 'yellow'}
+    foods.append(food)
+
+for food in foods[:5]:
+    if food['tye'] == 'banana':
+        food['taste'] = 'sour'
+        food['color'] = 'green'
+for food in foods[:10]:
+    print(food)
+
+# A List in a Dictionary pizza.py
+
+# Store information about a pizza beeing ordered
+pizza = {
+    'crust': 'thick',
+    'toppings': ['mushroom', 'extra cheese'],
+}
+# summarize the order
+print(f"You ordered {pizza['crust']}-crust pizza with the following toppings:")
+for topping in pizza['toppings']:
+    print(topping)
