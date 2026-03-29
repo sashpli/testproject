13571d011a44e920653daa9d1b51625e47183a61
Sasa Pliso <sasapliso@icloud.com>
2026-03-29 12:17:37 +0200

    while loops

--- user_input_while_loops.py
+++ user_input_while_loops.py
@@ -43,25 +43,56 @@
 # else:
 #     print("\nYour table is ready")
 
-print("\n")
-
-# While Loop
-
-current_number = 1
-while current_number <= 5:
-    print(current_number)
-    current_number += 1
+# print("\n")
+#
+# # While Loop
+#
+# current_number = 1
+# while current_number <= 5:
+#     print(current_number)
+#     current_number += 1
 
 # Choosing when to quit
+
+#
+# prompt = "\nTell me something and I will reapeat it back to you:"
+# prompt += "\nEnter 'quit' to end the program. "
+#
+# message = ""
+# while message != "quit":
+#     message = input(prompt)
+#     if message != "quit":
+#         print(message)
+
+
+# Flags
+
+prompt = "\nTell me something and I will reapeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == "quit":
+        active = False
+    else:
+        print(message)
+
+
+# Using a break
 
 
 prompt = "\nTell me something and I will reapeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
 
-message = ""
-while message != "quit":
+active = True
+while active:
     message = input(prompt)
-    if message != "quit":
+
+    if message == "quit":
+        active = False
+    else:
         print(message)
 
 
