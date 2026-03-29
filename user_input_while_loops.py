4c49f7ecf5629ebc481f346d269b57f0f5969f43
Sasa Pliso <sasapliso@icloud.com>
2026-03-29 12:00:51 +0200

    while loops

--- user_input_while_loops.py
+++ user_input_while_loops.py
@@ -55,3 +55,13 @@ while current_number <= 5:
 # Choosing when to quit
 
 
+prompt = "\nTell me something and I will reapeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+message = ""
+while message != "quit":
+    message = input(prompt)
+    if message != "quit":
+        print(message)
+
+
