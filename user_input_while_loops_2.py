216d944b7c59e16ece568ad6c9e1f6120a075a02
Sasa Pliso <sasapliso@icloud.com>
2026-04-02 18:44:53 +0200

    user input and while loops

--- user_input_while_loops_2.py
+++ user_input_while_loops_2.py
@@ -1,3 +1,22 @@
 # User input input() function
 message = input("Tell me something, and I will repeat it back to you: ")
 print(message)
+
+name = input("Please enter your name: ")
+print(f"Hello, {name}!")
+
+prompt = "if you tell us who you are, we can personalize the message you see."
+prompt += "\nWhat is your first name? "
+name = input(prompt)
+print(f"Hello {name.title()}")
+
+age = input("How old are you? ")
+age = int(age)
+print(f"You are {age} years old.")
+
+heigt = input("How tall are you in inches? ")
+heigt = int(heigt)
+if heigt >= 36:
+    print("You are tall enough to ride!")
+else:
+    print("You are to short for a ride!")
