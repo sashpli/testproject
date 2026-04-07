a53618955af4edd3fdab929d0bef9e6a1e6ea22e
Sasa Pliso <sasapliso@icloud.com>
2026-04-07 16:57:12 +0200

    user input and while loops

--- user_input_whille_loops_3.py
+++ user_input_whille_loops_3.py
@@ -4,3 +4,5 @@ message = input("Tell me something, and I will repeat it back to you: ")
 print(message)
 
 # greter.py
+name = input("Please enter your name: ")
+print(f"Hello {name}, welcome to python!")
