65742292b4ae09daceefe466ae5dbf75c690bf82
Sasa Pliso <sasapliso@icloud.com>
2026-04-07 18:06:44 +0200

    user input and while loops

--- user_input_whille_loops_3.py
+++ user_input_whille_loops_3.py
@@ -7,4 +7,24 @@ print(message)
 name = input("Please enter your name: ")
 print(f"Hello {name}, welcome to python!")
 
-# ok das ist ein Test
+message = "if you tell us who you are, we can personalize the message you see."
+message += "\nWhat is your first name? "
+name = input(message)
+print(f"Hello {name.title()}, welcome to pycharm!")
+
+# using int for numerical input
+
+heigth = input("How tall are you? ")
+heigth = int(heigth)
+if heigth >= 36:
+    print("You are tall enought to ride")
+else:
+    print("You are to short to ride")
+
+# even or odd
+number = input("Enter a number, and Ill tell you if its even or odd: ")
+number = int(number)
+if number % 2 == 0:
+    print(f"{number} is even")
+else:
+    print(f"{number} is odd")
