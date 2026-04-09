d6717818253bd250a5d339fc36b6858197ef53ae
Sasa Pliso <sasapliso@icloud.com>
2026-04-09 17:34:27 +0200

    user input and while loops

--- user_input_whille_loops_3.py
+++ user_input_whille_loops_3.py
@@ -114,7 +114,27 @@ while active:
     else:
         print(message)
 
+# Using break to Exit loop
+prompt = "\nPlease enter the name of a city you have visited:"
+prompt += "\n(Enter 'quit' when you are finished)"
+while True:
+    city = input(prompt)
 
+    if city == "quit":
+        break
+    else:
+        print(f"I'd love to go to {city.title()}!")
+
+# Using continue in a loop
+current_number = 0
+while current_number < 10:
+    current_number += 1
+    if current_number % 2 == 0:
+        continue
+    else:
+        print(current_number)
+
+# Avoiding infinite Loops
 
 
 
