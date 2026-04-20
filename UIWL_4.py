bd78ab3c7557fcfdf15c127ada808fe71b7c863f
Sasa Pliso <sasapliso@icloud.com>
2026-04-20 16:57:54 +0200

    user input and while loops

---
+++ UIWL_4.py
@@ -0,0 +1,14 @@
+# parrot.py
+message = input("Tell me something and I will repeat it back to you: ")
+print(message)
+
+#greeter.py
+name = input("Please enter you name: ")
+print(f"Hello {name.title()}!")
+
+prompt = "If you tell us who you are, we can peronalize the message you see. "
+prompt += "\nWhat is you first name? "
+name = input(prompt)
+print(f"Hello {name}")
+
+# accepting numerical input
