49828f65d8671a0791f9b1d5e01c4bcfad269cd5
Sasa Pliso <sasapliso@icloud.com>
2026-04-20 19:07:47 +0200

    user input and while loops

--- Test2.py
+++ Test2.py
@@ -16,15 +16,33 @@
 #     else:
 #         print("Das is nicht richtig!")
 
-prompt = "Was is die Hauptstadt von der Schweiz? "
-active = True
-while active:
+# prompt = "Was is die Hauptstadt von der Schweiz? "
+# active = True
+# while active:
+#     message = input(prompt)
+#     if message == "zürich":
+#         active = False
+#         print("Zürich ist die größte Stadt, aber nicht die Hauptstadt, das Programm wird beendet")
+#     elif message == "Bern":
+#         active = False
+#         print(f"Genau {message.title()} ist die Hauptstadt von der Schweiz?")
+#     else:
+#         print(f"{message.title()} ist nicht die Hauptstadt!")
+
+# prompt = "Wie viel EUR verdienst du? "
+# while True:
+#     salary = input(prompt)
+#     if int(salary) <= 2000:
+#         print(f"{salary} ist sehr wenig, dieses Programm schließt jetzt")
+#         break
+#     else:
+#         print(f"Geht noch mehr? Schreib eine Zahl! ")
+
+# 7-4 Pizza Toppings
+prompt = "Enter a pizza topping "
+message = ""
+while message != "quit":
     message = input(prompt)
-    if message == "zürich":
-        active = False
-        print("Zürich ist die größte Stadt, aber nicht die Hauptstadt, das Programm wird beendet")
-    elif message == "Bern":
-        active = False
-        print(f"Genau {message.title()} ist die Hauptstadt von der Schweiz?")
-    else:
-        print(f"{message.title()} ist nicht die Hauptstadt!")
+    print(f"{message.title()} added to your pizza!")
+
+prompt =
